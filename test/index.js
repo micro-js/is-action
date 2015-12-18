ec08323de4bef048fc8390e3b1d70196a419c4a3
Josh Taylor <joshrtay@gmail.com>
2015-12-18 09:58:53 -0800

    better type checking

--- test/index.js
+++ test/index.js
@@ -14,7 +14,13 @@ test('should return true for action', function (t) {
   t.end()
 })
 
-test('should return false if not action', function (t) {
+
+test('should return false if action like', function (t) {
   t.equal(isAction({type: 'action', then: function () {}}), false)
+  t.end()
+})
+
+test('should return false if no type', function (t) {
+  t.equal(isAction({payload: 'foo'}), false)
   t.end()
 })
